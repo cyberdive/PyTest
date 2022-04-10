854b6221343683bba2a1bb94cf3057f2698e4e0f
Patrick <tellier@gmail.com>
2022-04-10 10:39:19 +0200

    add more examples

--- helloworld.py
+++ helloworld.py
@@ -3,3 +3,7 @@ print(1 + 2)
 print(7 * 6)
 print()
 print("The end", "or is it?", "keep watching to learn more about Python", 3)
+greetings="Welcome"
+print(0/5)
+name = input ("Quel est votre nom ?")
+print(greetings, name)
